6cc10965213dabe4c83fe3b1974ec2a89076c8c5
sjsyz <57545155+sjsyz@users.noreply.github.com>
2020-10-08 11:18:23 -0500

    Use SAFE_DIVIDE
    query will error out with division by zero errors in certain situations, SAFE_DIVIDE should be used instead

--- dashboards/system_tables/sql/reservation_utilization_week.sql
+++ dashboards/system_tables/sql/reservation_utilization_week.sql
@@ -70,9 +70,9 @@ SELECT
   jbo.reservation_id,
   -- Slot usage is calculated by aggregating total_slot_ms for all jobs
   -- in the last week and dividing by the number of milliseconds in a week
-  SUM(jbo.total_slot_ms) / (1000 * 60 * 60 * 24 * 7) AS average_weekly_slot_usage,
+  SAFE_DIVIDE(SUM(jbo.total_slot_ms), (1000 * 60 * 60 * 24 * 7)) AS average_weekly_slot_usage,
   AVG(rsc.slot_capacity) AS average_reservation_capacity,
-  (SUM(jbo.total_slot_ms) / (1000 * 60 * 60 * 24 * 7)) / AVG(rsc.slot_capacity)
+  SAFE_DIVIDE((SAFE_DIVIDE(SUM(jbo.total_slot_ms), (1000 * 60 * 60 * 24 * 7))), AVG(rsc.slot_capacity))
     AS reservation_utilization,
   lsc.slot_capacity AS latest_capacity
 FROM
